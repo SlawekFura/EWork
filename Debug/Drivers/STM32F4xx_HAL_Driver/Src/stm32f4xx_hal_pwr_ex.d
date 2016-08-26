2e441e0a5801d5c0deebca0ada8a924394c1cfbe
Slawek <slawek.fura@gmail.com>
2016-08-26 17:04:15 +0200

    Initial commit

---
+++ Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.d
@@ -0,0 +1,86 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Inc/stm32f4xx_hal_conf.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Inc/mxconstants.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xc.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/CMSIS/Include/core_cmSimd.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Inc/stm32f4xx_hal_conf.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Inc/mxconstants.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xc.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/CMSIS/Include/core_cm4.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/CMSIS/Include/core_cmSimd.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+C:/Slawek/Mikrokontrolery/ARM_workspace/I2C/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
---
+++ Src/main.c
@@ -0,0 +1,362 @@
+/**
+  ******************************************************************************
+  * File Name          : main.c
+  * Description        : Main program body
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2016 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* USER CODE BEGIN Includes */
+#include <math.h>
+/* USER CODE END Includes */
+
+/* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
+float Zaxis_g = 0;
+float retAccel = 0;
+double ang;
+int16_t angVelocity;
+
+
+SPI_HandleTypeDef hspi1;
+
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+void Error_Handler(void);
+static void MX_GPIO_Init(void);
+static void MX_I2C1_Init(void);
+static void MX_SPI1_Init(void);
+
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+
+/* USER CODE END PFP */
+
+/* USER CODE BEGIN 0 */
+#define LSM303_ACC_ADDRESS (0x19 << 1) // adres akcelerometru: 0011 001x
+#define LSM303_ACC_CTRL_REG1_A 0x20 // rejestr ustawien 1
+// CTRL_REG1_A = [ODR3][ODR2][ODR1][ODR0][LPEN][ZEN][YEN][XEN]
+#define LSM303_ACC_Z_ENABLE 0x04 // 0000 0100
+#define LSM303_ACC_100HZ 0x50 // 0101 0000
+#define LSM303_ACC_Z_H_A 0x2D // wyzszy bajt danych osi Z
+#define LSM303_ACC_RESOLUTION 2
+
+
+#define L3GD20_CTRL_REG1_A 0x20 // rejestr ustawien 1
+#define L3GD20_ENABLE 0x08 // 0000 0100
+#define L3GD20_Z_ENABLE 0x04 // 0000 0100
+#define L3GD20_190HZ 0x50 // 0101 0000
+#define L3GD20_Z_H_A 0x2D // wyzszy bajt danych osi Z
+#define L3GD20_WHO_AM_I 0x0F
+
+struct SPI_L3GD20{
+	uint8_t REG_CTRL_REG1_llkA_WRITE = 0x00100000;
+	uint8_t * pREG_CTRL_REG1_A_WRITE = &REG_CTRL_REG1_A_WRITE;
+
+	uint8_t REG_CTRL_REG1_A_READ = 0x10100000;
+	uint8_t * pREG_CTRL_REG1_A_READ = &REG_CTRL_REG1_A_READ;
+
+	uint8_t WHO_AM_I_READ = 0b10001111;
+	uint8_t * pWHO_AM_I_READ = &WHO_AM_I_READ;
+
+	uint8_t Settings = 0b01101111;
+	uint8_t * pSettings = &Settings;
+
+	uint8_t AxisZVal;
+	uint8_t * pAxisZVal;
+};
+/* USER CODE END 0 */
+
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration----------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_I2C1_Init();
+  MX_SPI1_Init();
+
+  /* USER CODE BEGIN 2 */
+
+  uint8_t Settings = LSM303_ACC_Z_ENABLE | LSM303_ACC_100HZ;
+  uint8_t SettingsGyro =  0b01101111;
+
+  HAL_I2C_Mem_Write(&hi2c1, LSM303_ACC_ADDRESS, LSM303_ACC_CTRL_REG1_A, 1, &Settings, 1, 100);
+
+  uint8_t sendToGyro[4]={L3GD20_CTRL_REG1_A, SettingsGyro, L3GD20_Z_H_A | 128, L3GD20_WHO_AM_I | 128};
+  uint8_t *sendData = sendToGyro;
+  uint8_t getFromGyro = 0;
+  uint8_t *getData;// = &getFromGyro;
+
+
+  HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,RESET);
+  HAL_SPI_Transmit(&hspi1,sendData,1,100);
+  HAL_SPI_Transmit(&hspi1,sendData + 1,1,100);
+  HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,SET);
+
+  uint8_t Data = 0; // Zmienna do bezposredniego odczytu z akcelerometru
+  int16_t Zaxis = 0; // Zawiera przeksztalcona forme odczytanych danych
+
+  uint8_t a=0;
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+  /* USER CODE END WHILE */
+
+  /* USER CODE BEGIN 3 */
+	 HAL_I2C_Mem_Read(&hi2c1, LSM303_ACC_ADDRESS, LSM303_ACC_Z_H_A, 1, &Data, 1, 100);
+	 Zaxis = Data << 8;
+	 HAL_I2C_Mem_Read(&hi2c1, LSM303_ACC_ADDRESS, LSM303_ACC_Z_H_A, 1, &Data, 1, 100);
+	 Zaxis |= Data;
+	 Zaxis_g = (((double)Zaxis*LSM303_ACC_RESOLUTION)/(double)INT16_MAX);
+
+	HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,RESET);
+	HAL_SPI_Transmit(&hspi1,sendData + 2,1,100);
+	HAL_SPI_Receive(&hspi1,getData,1,100);
+	HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,SET);
+	angVelocity = (*getData)<<8;
+
+  }
+  /* USER CODE END 3 */
+
+}
+
+/** System Clock Configuration
+*/
+void SystemClock_Config(void)
+{
+
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = 16;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 192;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV6;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV2;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+/* I2C1 init function */
+static void MX_I2C1_Init(void)
+{
+
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
+
+/* SPI1 init function */
+static void MX_SPI1_Init(void)
+{
+
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
+
+/** Configure pins as 
+        * Analog 
+        * Input 
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+static void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_SET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
+
+  /*Configure GPIO pin : PE3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_3;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PD12 */
+  GPIO_InitStruct.Pin = GPIO_PIN_12;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+double showAngle(double const var)
+{
+	if(var>1)
+		 ang = asin(1)*360/(2*3.14);
+	else if (Zaxis_g < -1)
+		 ang = asin(-1)*360/(2*3.14);
+	else
+		 ang = asin(var)*360/(2*3.14);
+	return ang;
+}
+
+//getRadDegPerSec(char * mode,int16_t *getVal){
+//	HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,RESET);
+//	HAL_SPI_Transmit(&hspi1,getVal + 2,1,100);
+//	HAL_SPI_Receive(&hspi1,getVal,1,100);
+//	HAL_GPIO_WritePin(GPIOE,GPIO_PIN_3,SET);
+//}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1) 
+  {
+  }
+  /* USER CODE END Error_Handler */ 
+}
+
+#ifdef USE_FULL_ASSERT
+
+/**
+   * @brief Reports the name of the source file and the source line number
+   * where the assert_param error has occurred.
+   * @param file: pointer to the source file name
+   * @param line: assert_param error line source number
+   * @retval None
+   */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+    ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+
+}
+
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
