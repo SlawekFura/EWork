94418d932d546f14181282013589b6e5eedaffb3
Slawek <slawek.fura@gmail.com>
2016-08-26 20:06:12 +0200

    + Includes

---
+++ Includes/I2C_SPI.h
@@ -0,0 +1,48 @@
+/*
+ * I2C_SPI.h
+ *
+ *  Created on: 26 sie 2016
+ *      Author: Mariusz
+ */
+
+#ifndef I2C_SPI_H_
+#include "stm32f4xx_hal.h"
+
+#define I2C_SPI_H_
+
+
+#define LSM303_ACC_ADDRESS (0x19 << 1) // adres akcelerometru: 0011 001x
+#define LSM303_ACC_CTRL_REG1_A 0x20 // rejestr ustawien 1
+#define LSM303_ACC_Z_ENABLE 0x04 // 0000 0100
+#define LSM303_ACC_100HZ 0x50 // 0101 0000
+#define LSM303_ACC_Z_H_A 0x2D // wyzszy bajt danych osi Z
+#define LSM303_ACC_RESOLUTION 2
+
+const uint8_t AccSettings = LSM303_ACC_Z_ENABLE | LSM303_ACC_100HZ;
+
+
+#define L3GD20_GYRO_CTRL_REG1 0x20 // rejestr ustawien 1
+#define L3GD20_GYRO_190HZ_25BW 0x50 // 0101 0000
+#define L3GD20_GYRO_ENABLE 0x08 // 0000 1000
+#define L3GD20_GYRO_ZXY_ENABLE 0x07 // 0000 0100
+#define L3GD20_GYRO_Z_H 0x2D // wyzszy bajt danych osi Z
+#define L3GD20_GYRO_Z_L 0x2C // wyzszy bajt danych osi Z
+#define L3GD20_GYRO_WHO_AM_I 0x0F
+
+const uint8_t GyroSettings = L3GD20_GYRO_190HZ_25BW | L3GD20_GYRO_ENABLE /
+							 L3GD20_GYRO_ZXY_ENABLE;
+const uint8_t sendToGyro[4]={L3GD20_GYRO_CTRL_REG1, GyroSettings, L3GD20_GYRO_Z_H | 128,
+							L3GD20_GYRO_Z_H | 128, L3GD20_GYRO_WHO_AM_I | 128};
+uint8_t * pSendSPI[4] = sendToGyro;
+
+void getDataACC(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint8_t *pDataGet uint32_t Timeout);
+void getDataSPI(SPI_HandleTypeDef *hspi, uint8_t * RegAddress, uint8_t *pDataGet uint32_t Timeout);
+
+//void getDataGYRO(   )
+
+void initAccI2C();
+void initGyroSPI();
+
+
+
+#endif /* I2C_SPI_H_ */
